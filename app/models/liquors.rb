d17c3a3762b16f0d7276ca23bab52d13fe239552
subaru-hello <octosubaru0926@gmail.com>
2022-11-06 22:04:37 +0900

    remove typo model

--- app/models/liquors.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Liquor < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 20 }
-  validates :price, presence: true
-  validates :percentage, precsence: true
-  validates :amount, presence: true
-end
