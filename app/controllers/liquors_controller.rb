191d835e53b23c61a0a2137a4cbb69769e58f743
subaru-hello <octosubaru0926@gmail.com>
2022-11-06 18:19:14 +0900

    new create action

--- app/controllers/liquors_controller.rb
+++ app/controllers/liquors_controller.rb
@@ -4,4 +4,28 @@ class LiquorsController < ApplicationController
   def index
     @liquors = Liquor.all
   end
+
+  def show
+    @liquor = Liquor.find(params[:id])
+  end
+
+  def new
+    @liquor = Liquor.new
+  end
+
+  def create
+    @liquor = Liquor.new(liquor_params)
+
+    if @liquor.save
+      redirect_to @liquor
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def liquor_params
+    params.require(:liquor).permit(:name, :price, :percentage, :amount)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/liquors', to: 'liquors#index'
   root 'home#index'
 
+  resources :liquors, only: %w[index show new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
