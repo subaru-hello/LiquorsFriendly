798a421721e8fbe7aac683527522a326a39eb99f
subaru-hello <octosubaru0926@gmail.com>
2022-12-18 14:07:55 +0900

    add comment#new route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :liquors do
-    resources :comments, only: %i[create destroy]
+    resources :comments, only: %i[new create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
