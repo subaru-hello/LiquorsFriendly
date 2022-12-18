e78b318a12fa1ad3a48d39e2573baffa4a1425f5
subaru-hello <octosubaru0926@gmail.com>
2022-12-18 14:08:18 +0900

    add new action and fix create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,30 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  def new
+    @liquor = Liquor.find(params[:liquor_id])
+    @comment = User.first.comments.new(comment_params)
+  end
+
   def create
     @liquor = Liquor.find(params[:liquor_id])
-    @comment = @liquor.comments.create(comment_params)
-    redirect_to liquor_path(@liquor)
+    @comment = User.first.comments.new(comment_params)
+
+    return unless @comment.save
+
+    redirect_to liquor_path(@liquor), notice: "#{@comment.title}を追加しました。"
   end
 
   def destroy
     @liquor = Liquor.find(params[:liquor_id])
     @comment = @liquor.comments.find(params[:id])
     @comment.destroy
-    redirect_to liquor_path(@liquor), status: :see_other
+    redirect_to liquor_path(@liquor), status: :see_other, notice: "#{@comment.title}を削除しました。"
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :status)
+    params.require(:comment).permit(:title, :body, :status, :liquor_id)
   end
 end
